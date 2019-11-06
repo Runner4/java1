f47fd89073db61601f24578f9916bdaee7da302c
Владислав <pvvvit@mail.ru>
2019-11-06 23:09:14 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/java1/lessons/helloworld/Task2.java
+++ src/ru/progwards/java1/lessons/helloworld/Task2.java
@@ -1,5 +1,8 @@
 package ru.progwards.java1.lessons.helloworld;
 
 public class Task2 {
-
+    public static void subtraction(int x, int y)
+    public static void main(String[] args) {
+        subtraction(x = 89, y = 55);
+    }
 }
