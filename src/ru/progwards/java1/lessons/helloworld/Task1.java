e7433312294e7ad7d203cc48cfd7683ac1718920
Владислав <pvvvit@mail.ru>
2019-11-07 20:25:57 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/java1/lessons/helloworld/Task1.java
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -1,18 +1,7 @@
 package ru.progwards.java1.lessons.helloworld;
 
 public class Task1 {
-    //1.1 Реализовать функцию public static void main(String[] args) в которой следует: создать три строковые переменные со значениями:
-    //
-    //        "Хорошо идут дела"
-    //        "Изучаю Java я!"
-    //        " "
-    //
-    //вывести на консоль, используя только эти переменные и функции System.out.print(), System.out.println() следующий текст:
-    //Хорошо идут дела
-    //Изучаю Java я!
-    //Хорошо идут дела Изучаю Java я!
-    //Изучаю Java я! Хорошо идут дела
-    static void Task1() {
+        static void Task1() {
         String x;
         String y;
         String z;
