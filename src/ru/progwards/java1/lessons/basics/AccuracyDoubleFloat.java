bff8b2feee91d4ac2e11b0090f21f19967ee04be
Владислав <pvvvit@mail.ru>
2019-11-19 22:38:08 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -15,8 +15,6 @@ public class AccuracyDoubleFloat {
         return volumeBallDouble(radius)-volumeBallFloat((float)radius);
     }
     public static void main(String[] args) {
-        //double R = 6371.2;
-        //float R = 6371.2f;
         System.out.println(volumeBallDouble(1));
         System.out.println(volumeBallFloat(1));
         System.out.println(calculateAccuracy(1));
