e238731f11d639d4f71b19c8cadc2d28796acea6
Владислав <pvvvit@mail.ru>
2019-11-20 20:26:51 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -2,21 +2,21 @@ package ru.progwards.java1.lessons.basics;
 
 public class AccuracyDoubleFloat {
     public static double volumeBallDouble(double radius){
-        double Pi = 3.14;
-        double R = 6371.2;
-        return (4.0/3.0)*(Pi)*(R*R*R);
+        //double Pi = 3.14;
+        //double R = radius;
+        return (4.0/3.0)*3.14*(radius*radius*radius);
     }
      public static float volumeBallFloat(float radius){
-        float Pi = 3.14f;
-        float R = 6371.2f;
-        return (float) (4.0f/3.0f)*Pi*(R*R*R);
+       // float Pi = 3.14f;
+        //float R = radius;
+        return (float) (4.0/3.0)*3.14f*(radius*radius*radius);
     }
     public static double calculateAccuracy(double radius){
         return volumeBallDouble(radius)-volumeBallFloat((float)radius);
     }
     public static void main(String[] args) {
-        System.out.println(volumeBallDouble(1));
-        System.out.println(volumeBallFloat(1));
-        System.out.println(calculateAccuracy(1));
+        System.out.println(volumeBallDouble(6371.2));
+        System.out.println(volumeBallFloat(6371.2f));
+        System.out.println(calculateAccuracy(6371.2));
     }
 }
--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -2,9 +2,11 @@ package ru.progwards.java1.lessons.basics;
 
 public class ReverseDigits {
     public static int reverseDigits(int number){
+
         return (number%10*100)+(number/10%10*10)+(number/100);
     }
     public static void main(String[] args) {
+
         System.out.println(reverseDigits(724));
     }
 }
