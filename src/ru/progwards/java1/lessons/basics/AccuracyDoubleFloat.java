3fbd4d9e800de6351c0c21a4daa2f0a19c880209
Владислав <pvvvit@mail.ru>
2019-11-22 20:00:27 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -1,22 +1,44 @@
 package ru.progwards.java1.lessons.basics;
 
 public class AccuracyDoubleFloat {
-    public static double volumeBallDouble(double radius){
+  //  public static double volumeBallDouble(double radius){
         //double Pi = 3.14;
         //double R = radius;
-        return (4.0/3.0)*3.14*(radius*radius*radius);
-    }
-     public static float volumeBallFloat(float radius){
+    //    return (4.0/3.0)*3.14*(radius*radius*radius);
+    //}
+     //public static float volumeBallFloat(float radius){
        // float Pi = 3.14f;
         //float R = radius;
-        return (float) (4.0/3.0)*3.14f*(radius*radius*radius);
+       // return (float) (4.0/3.0)*3.14f*(radius*radius*radius);
+    //}
+    //public static double calculateAccuracy(double radius){
+      //  return volumeBallDouble(radius)-volumeBallFloat((float)radius);
+    //}
+    //public static void main(String[] args) {
+      //  System.out.println(volumeBallDouble(6371.2));
+        //System.out.println(volumeBallFloat(6371.2f));
+        //System.out.println(calculateAccuracy(6371.2));
+    //}
+  final static double pi = 3.14;
+    //final static double earthR = 6_371.2;
+
+    public static double volumeBallDouble(double radius) {
+        return pi * radius * radius * radius * 4 / 3;
     }
-    public static double calculateAccuracy(double radius){
-        return volumeBallDouble(radius)-volumeBallFloat((float)radius);
+
+    public static float volumeBallFloat(float radius) {
+        return (float)pi * radius * radius * radius * 4 / 3;
     }
+
+    public static double calculateAccuracy(double radius) {
+        return volumeBallDouble(radius) - volumeBallFloat((float)radius);
+    }
+
     public static void main(String[] args) {
+       // System.out.println("Volume of R=1(d): " + volumeBallDouble(1));
+        //System.out.println("Volume of R=1(f): " + volumeBallFloat(1));
         System.out.println(volumeBallDouble(6371.2));
-        System.out.println(volumeBallFloat(6371.2f));
+        System.out.println(volumeBallFloat((float)6371.2));
         System.out.println(calculateAccuracy(6371.2));
     }
 }
---
+++ src/ru/progwards/pvv/Test3_1.java
@@ -0,0 +1,11 @@
+package ru.progwards.pvv;
+
+public class Test3_1 {
+    static int addAsStrings(int n1, int n2){
+        int a = Integer.parseInt(Integer.toString(n1) + Integer.toString(n2));
+        return a;
+    }
+    public static void main(String[] args) {
+        System.out.println(addAsStrings(73,93));
+    }
+}
