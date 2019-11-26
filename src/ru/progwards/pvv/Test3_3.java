d5c93eb4d9953a205ac826a19a7646e6877e2a18
Владислав <pvvvit@mail.ru>
2019-11-26 19:00:06 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/pvv/Test3_3.java
+++ src/ru/progwards/pvv/Test3_3.java
@@ -5,7 +5,7 @@ public class Test3_3 {
         long fact = 1L;
         if (n!=0L) {
             for (long c = 1L; c<=n; c++){
-                fact *=c;
+                fact *=c; //fact*=c это сокращение для fact=fact*c
             }
         } else {
             fact = 1L;
@@ -13,8 +13,8 @@ public class Test3_3 {
         return fact;
     }
     //  static long factorial (long num) {
-  //      return (num == 0) ? 1 : num * factorial (num - 1);
-  //  }
+    //    return (num == 0) ? 1 : num * factorial (num - 1);
+   //   }
     public static void main(String[] args) {
         System.out.println(factorial(3));
     }
