295df269d4e280586d0980b1a6602a39f8b10c3a
Владислав <pvvvit@mail.ru>
2020-01-19 13:03:22 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -16,3 +16,25 @@ public String toString(),
 
 */
 
+public class Binary {
+
+    byte num;
+
+    public Binary(byte num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        result += (num >> 7) & 0b00000001;
+        result += (num >> 6) & 0b00000001;
+        result += (num >> 5) & 0b00000001;
+        result += (num >> 4) & 0b00000001;
+        result += (num >> 3) & 0b00000001;
+        result += (num >> 2) & 0b00000001;
+        result += (num >> 1) & 0b00000001;
+        result += (num >> 0) & 0b00000001;
+        return result;
+    }
+}
