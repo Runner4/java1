94c81636db70daa378d4908e24679942e64a04fc
Владислав <pvvvit@mail.ru>
2020-04-15 21:47:42 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/pvv/Test.java
+++ src/ru/progwards/pvv/Test.java
@@ -9,9 +9,11 @@ public class Test {
         double x = 5/2;
         double y = 5.234 % 2;
         int z = 724 % 10;
+        int i = (int)765.567;
         System.out.println(x);
         System.out.println(y);
         System.out.println(z);
+        System.out.println(i);
 
     }
 
--- src/ru/progwards/pvv/lesson3/Test3_1.java
+++ src/ru/progwards/pvv/lesson3/Test3_1.java
@@ -1,11 +1,12 @@
 package ru.progwards.pvv.lesson3;
 
 public class Test3_1 {
-    static int addAsStrings(int n1, int n2){
+    static int addAsStrings(int n1, int n2) {
         int a = Integer.parseInt(Integer.toString(n1) + Integer.toString(n2));
         return a;
     }
+
     public static void main(String[] args) {
-        System.out.println(addAsStrings(73,93));
+        System.out.println(addAsStrings(73, 93));
     }
 }
