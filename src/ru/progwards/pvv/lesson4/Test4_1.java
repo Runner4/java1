6cc6f83d1b82518527c315074afbcca99d8d8b3e
Владислав <pvvvit@mail.ru>
2020-04-16 16:02:52 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/pvv/lesson4/Test4_1.java
+++ src/ru/progwards/pvv/lesson4/Test4_1.java
@@ -11,29 +11,39 @@ public class Test4_1 {
     конструктор с параметрами String name, int age, вызывающий конструктор по умолчанию, и устанавливающий значения переменным name и age из соответствующих параметров;
     публичные функции String getName(), int getAge(), String getCountry(), возвращающие значения соответствующих переменных
      */
-    public class Person {
+    public static class Person {
         private String name;
         private int age;
         private String country;
-        public Person(){
+
+        public Person() {
             country = "RU";
         }
-        public Person (String name, int age){
+
+        public Person(String name, int age) {
             this();
             this.name = name;
             this.age = age;
         }
-        public String getName(){
+
+        public String getName() {
             return name;
         }
-        public int getAge(){
+
+        public int getAge() {
             return age;
         }
-        public String getCountry(){
+
+        public String getCountry() {
             return country;
         }
     }
-   // public static void main(String[] args) {
 
-    //}
+    public static void main(String[] args) {
+        Person p = new Person("Vladimir", 21);
+        System.out.println(p.getAge());
+        System.out.println(p.getName());
+        System.out.println(p.getCountry());
+
+    }
 }
