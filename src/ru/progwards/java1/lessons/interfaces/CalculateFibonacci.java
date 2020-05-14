c5406148507ff4bd9204fff5832dcc13a602e24f
Владислав <pvvvit@mail.ru>
2020-05-14 13:49:54 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -1,6 +1,6 @@
 package ru.progwards.java1.lessons.interfaces;
 
-public class CalculateFibonacci {
+/*public class CalculateFibonacci {
     private static CacheInfo lastFibo = new CacheInfo();
 
     public static int fiboNumber(int n) {
@@ -38,10 +38,45 @@ public class CalculateFibonacci {
     }
 
     public static void clearLastFibo() {
-        CacheInfo lastFibo = null;
+        lastFibo = null;
     }
 
     public static void main(String[] args) {
-        System.out.println(fiboNumber(8));
+        System.out.println(fiboNumber(29));
+    }
+}*/
+public class CalculateFibonacci {
+    private static CacheInfo lastFibo = new CacheInfo();
+    public static int fiboNumber(int n) {
+        lastFibo = new CacheInfo();
+        if (lastFibo.n == n) {
+            return lastFibo.fibo;
+        }
+        int a = 1;
+        int b = 0;
+        for (int i = 0; i < n; i++) {
+            CacheInfo.fibo = a + b;
+            a = b;
+            b = CacheInfo.fibo;
+        }
+        System.out.println(CacheInfo.fibo);
+        CalculateFibonacci.lastFibo.n = n;
+        return CacheInfo.fibo;
+    }
+    public static class CacheInfo {
+        public static int n;
+        public static int fibo;
+    }
+    public static CacheInfo getLastFibo() {
+        return lastFibo;
+    }
+    public static void clearLastFibo() {
+        lastFibo = null;
+    }
+
+    public static void main(String[] args) {
+        fiboNumber(29);
+        System.out.println(CacheInfo.fibo);
+        System.out.println(CacheInfo.n);
     }
 }
---
+++ src/ru/progwards/pvv/TestCalculateFibonacci.java
@@ -0,0 +1,37 @@
+package ru.progwards.pvv;
+
+public class TestCalculateFibonacci {
+    private static CacheInfo lastFibo = new CacheInfo();
+    public static int fiboNumber(int n) {
+        lastFibo = new CacheInfo();
+        if (lastFibo.n == n) {
+            return lastFibo.fibo;
+        }
+        int a = 1;
+        int b = 0;
+        for (int i = 0; i < n; i++) {
+            CacheInfo.fibo = a + b;
+            a = b;
+            b = CacheInfo.fibo;
+        }
+        System.out.println(CacheInfo.fibo);
+        TestCalculateFibonacci.lastFibo.n = n;
+        return CacheInfo.fibo;
+    }
+    public static class CacheInfo {
+        public static int n;
+        public static int fibo;
+    }
+    public static CacheInfo getLastFibo() {
+        return lastFibo;
+    }
+    public static void clearLastFibo() {
+        lastFibo = null;
+    }
+
+    public static void main(String[] args) {
+        fiboNumber(29);
+        System.out.println(CacheInfo.fibo);
+        System.out.println(CacheInfo.n);
+    }
+}
