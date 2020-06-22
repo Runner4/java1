e20e8fa2b98fe0c305d391b0ae72a333f0611687
Vladislav <pvvvit@mail.ru>
2020-06-22 21:36:54 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/java1/lessons/io1/LineCount.java
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -6,13 +6,13 @@ package ru.progwards.java1.lessons.io1;
  */
 
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class LineCount {
 
     public static int calcEmpty(String fileName) {
-        int count = 0; //подсчет строк
+        int count = 0; //количество строк
         try {
             FileReader reader = new FileReader(fileName);
             Scanner scanner = new Scanner(reader);
@@ -37,26 +37,6 @@ public class LineCount {
 
     public static void main(String[] args) throws IOException {
         System.out.println(calcEmpty("src\\ru\\progwards\\java1\\lessons\\io1\\H10.1.txt"));
+        System.out.println(calcEmpty("src\\ru\\progwards\\java1\\lessons\\io1\\H10,1.txt"));
     }
-//        int result = 0;
-//        int resultOnError = -1;
-//        String searchString = "";
-//
-//        try (FileReader f = new FileReader(fileName); Scanner s = new Scanner(f)) {
-//            while (s.hasNextLine()) {
-//                String str = s.nextLine();
-//                if (str.compareTo(searchString) == 0) result++;
-//            }
-//        } catch (FileNotFoundException e) {
-//            result = resultOnError;
-//        } catch (IOException e) {
-//            result = resultOnError;
-//        }
-//        return result;
-//    }
-//
-//    public static void main(String[] args) {
-//        System.out.println(calcEmpty("java1.iml"));
-//        System.out.println(calcEmpty("java1-.iml"));
-//    }
 }
