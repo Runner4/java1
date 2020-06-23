52e0219afc4b8051df92395b4f244b29cbd79a36
Vladislav <pvvvit@mail.ru>
2020-06-23 22:12:30 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -10,47 +10,86 @@ public static void codeFile(String inFileName, String outFileName, char[] code, 
 В случае ошибок, в файл с именем logName вывести название ошибки через метод класса Exception - getMessage()
 */
 
-import java.io.FileReader;
+import java.io.*;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Coder {
 
-    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
-        try {
-            //запись ошибок в лог
-            FileWriter error = new FileWriter(logName);
-            //копия файла
-            FileWriter in = new FileWriter(outFileName);
-            try {
-                //чтение
-                FileReader reader = new FileReader(inFileName);
-                Scanner scanner = new Scanner(reader);
-                while (scanner.hasNextLine()) {
-                    String strFromFile = scanner.nextLine();
-                    for (int i = 0; i < strFromFile.length(); i++) {
-                        int a = 0;
-                        for (char value : code) {
-                            a += value;
-                            in.write("" + Integer.valueOf(a) + "\n");   //столбец
-                        }
-                    }
-                }
-                in.close();
-                scanner.close();
-
-            } catch (Exception e) {
-                error.write(e.getMessage());
-                error.close();
-                throw new RuntimeException(e);
-            }
-        } catch (
-                Exception e) {
-            throw new RuntimeException(e);
+//    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
+//        try {
+//            //запись ошибок в лог
+//            FileWriter error = new FileWriter(logName);
+//            //копия файла
+//            FileWriter in = new FileWriter(outFileName);
+//            try {
+//                //чтение
+//                FileReader reader = new FileReader(inFileName);
+//                Scanner scanner = new Scanner(reader);
+//                while (scanner.hasNextLine()) {
+//                    String strFromFile = scanner.nextLine();
+//                    for (int i = 0; i < strFromFile.length(); i++) {
+//                        int a = 0;
+//                        for (char value : code) {
+//                            a += value;
+//                            in.write("" + Integer.valueOf(a) + "\n");   //столбец
+//                        }
+//                    }
+//                }
+//                in.close();
+//                scanner.close();
+//
+//            } catch (Exception e) {
+//                error.write(e.getMessage());
+//                error.close();
+//                throw new RuntimeException(e);
+//            }
+//        } catch (
+//                Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
+public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
+    FileInputStream fIn = null;
+    BufferedInputStream bIn = null;
+    FileOutputStream fOut = null;
+    BufferedOutputStream bOut = null;
+    try {
+        fIn = new FileInputStream(inFileName);
+        bIn = new BufferedInputStream(fIn);
+        fOut = new FileOutputStream(outFileName);
+        bOut = new BufferedOutputStream(fOut);
+        int i;
+        while ((i = bIn.read()) != -1) {
+            bOut.write(code[i]);
         }
-
+    } catch (Throwable e) {
+        FileWriter fEOut = null;
+        BufferedWriter bEOut = null;
+        try {
+            fEOut = new FileWriter(logName, true);
+            bEOut = new BufferedWriter(fEOut);
+            bEOut.write(e.getMessage());
+        } catch (Throwable e2) {
+        } finally {
+            try {
+                if (bEOut != null) bEOut.close();
+                if (fEOut != null) fEOut.close();
+            } catch (Throwable e3) {
+            }
+        }
+    } finally {
+        try {
+            if (bOut != null) bOut.close();
+            if (fOut != null) fOut.close();
+            if (bIn != null) bIn.close();
+            if (fIn != null) fIn.close();
+        } catch (Throwable e) {
+        }
     }
+}
 
     public static void main(String[] args) throws IOException {
         char[] code = new char[256];
