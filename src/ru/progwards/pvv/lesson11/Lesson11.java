4d1672ee9f5f83fbfd17a255009e1f1376ceef45
Vladislav <pvvvit@mail.ru>
2020-07-02 20:58:00 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

--- src/ru/progwards/pvv/lesson11/Lesson11.java
+++ src/ru/progwards/pvv/lesson11/Lesson11.java
@@ -55,4 +55,27 @@ public class Lesson11 {
             }
         }
     }
+
+    /*
+    Реализуйте метод с сигнатурой public String invertWords(String sentence),
+    который переставляет слова, в полученной фразе. В качестве исходного
+    разделителя использовать пробел. В качестве соединительного точку.
+    Например,
+    invertWords("Буря мглою небо кроет") должен вернуть
+    "кроет.небо.мглою.Буря"
+    */
+
+    public static String invertWords(String sentence) {
+        String[] splitted = sentence.split(" "); // разбиваем строку на массив строк, разделитель пробел
+        StringBuilder result = new StringBuilder(30); // создаём буфер на 30 символов
+        for (int i = splitted.length - 1; i >= 0; i--) {
+            result.append(splitted[i]); // переворачиваем массив строк
+            if (i>0) result.append("."); // добавляем в качестве разделителя .
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(invertWords("Буря мглою небо кроет"));
+    }
 }
