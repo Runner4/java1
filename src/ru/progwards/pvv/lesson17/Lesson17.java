f08753786b16abf449f0cb37d87cc67fe2713e23
Vladislav <pvvvit@mail.ru>
2020-10-26 21:46:42 +0300

    Revert "HelloWorld start main"
    This reverts commit a8414287

---
+++ src/ru/progwards/pvv/lesson17/Lesson17.java
@@ -0,0 +1,38 @@
+package ru.progwards.pvv.lesson17;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Lesson17 {
+
+    // Создайте метод с сигнатурой String createFolder(String name),
+    // который создает каталог name (один уровень) в текущей папке и возвращает полный родителя текущего каталога
+    String createFolder(String name) {
+        File f = new File(name);
+        f.mkdir();
+        Path p = Paths.get("..");
+        return p.toAbsolutePath().normalize().toString();
+    }
+
+    // Реализовать метод с сигнатурой boolean replaceF(String name) который заменяет в файле все F на f,
+    // в случае ошибки вернуть false. Для реализации пользоваться методами java.nio.file.Files.
+
+    public static void main(String[] args) {
+        System.out.println(new Lesson17().createFolder("qwe"));
+    }
+
+    boolean replaceF(String name) {
+        String content, newContent;
+        try {
+            Path f = Paths.get(name);
+            content = Files.readString(f);
+            newContent = content.replaceAll("F", "f");
+            Files.writeString(f, newContent);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
